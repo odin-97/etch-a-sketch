51bf4500f301384974e1505a39210a631e87f24a
odin-97 <220430078+odin-97@users.noreply.github.com>
2025-11-13 14:57:00 +0100

    Create one row of square divs manually
    Testing out element creation and inserting into container to see how
    sizes and layout behave if inserting squares into the grid.

--- javascript.js
+++ javascript.js
@@ -1,2 +1,27 @@
-const gridElement = document.createElement('div');
-console.log(gridElement);
+const gridContainer = document.querySelector('.gridContainer');
+console.log(gridContainer);
+
+const gridElement1 = document.createElement('div');
+gridElement1.style.width = '25%';
+gridElement1.style.aspectRatio = '1 / 1';
+gridElement1.style.border = '2px solid green';
+gridContainer.appendChild(gridElement1);
+
+const gridElement2 = document.createElement('div');
+gridElement2.style.width = '25%';
+gridElement2.style.aspectRatio = '1 / 1';
+gridElement2.style.border = '2px solid green';
+gridContainer.appendChild(gridElement2);
+
+const gridElement3 = document.createElement('div');
+gridElement3.style.width = '25%';
+gridElement3.style.aspectRatio = '1 / 1';
+gridElement3.style.border = '2px solid green';
+gridContainer.appendChild(gridElement3);
+
+const gridElement4 = document.createElement('div');
+gridElement4.style.width = '25%';
+gridElement4.style.aspectRatio = '1 / 1';
+gridElement4.style.border = '2px solid green';
+gridContainer.appendChild(gridElement4);
+
