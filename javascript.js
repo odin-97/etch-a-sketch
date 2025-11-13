2046b406606e4ab69e917dca1d2a781080b0fb8c
odin-97 <220430078+odin-97@users.noreply.github.com>
2025-11-13 16:02:26 +0100

    Add styles to grid Elements via js
    Creates style rules and class via JavaScript once to easily apply to
    created square grid elemets instead of manually defining same style rules
    for each element every time.

--- javascript.js
+++ javascript.js
@@ -1,27 +1,35 @@
 const gridContainer = document.querySelector('.gridContainer');
-console.log(gridContainer);
+
+
+const style = document.createElement('style');
+document.head.appendChild(style)
+
+style.sheet.insertRule(`
+.gridItem {
+    width: 25%;
+    aspect-ratio: 1 / 1;
+    border: 2px solid blue;
+}`);
 
 const gridElement1 = document.createElement('div');
-gridElement1.style.width = '25%';
-gridElement1.style.aspectRatio = '1 / 1';
-gridElement1.style.border = '2px solid green';
+gridElement1.classList.add('gridItem');
 gridContainer.appendChild(gridElement1);
 
 const gridElement2 = document.createElement('div');
-gridElement2.style.width = '25%';
-gridElement2.style.aspectRatio = '1 / 1';
-gridElement2.style.border = '2px solid green';
+gridElement2.classList.add('gridItem')
 gridContainer.appendChild(gridElement2);
 
 const gridElement3 = document.createElement('div');
-gridElement3.style.width = '25%';
-gridElement3.style.aspectRatio = '1 / 1';
-gridElement3.style.border = '2px solid green';
+gridElement3.setAttribute('class', 'gridItem');
 gridContainer.appendChild(gridElement3);
 
 const gridElement4 = document.createElement('div');
-gridElement4.style.width = '25%';
-gridElement4.style.aspectRatio = '1 / 1';
-gridElement4.style.border = '2px solid green';
+gridElement4.className = 'gridItem';
 gridContainer.appendChild(gridElement4);
+
+const gridElement5 = document.createElement('div');
+gridElement5.classList.add('gridItem')
+gridContainer.appendChild(gridElement5);
+
+
 
