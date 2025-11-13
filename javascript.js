2280e2466aba52f49c3f9e25fb61d89bb4eebbda
odin-97 <220430078+odin-97@users.noreply.github.com>
2025-11-13 16:30:08 +0100

    Create rows and columns via for loop
    Allows to define number of elements per row via a variable instead of
    hardcoding magic number. Allows to create flexible gid size via user
    input for later implementation.

--- javascript.js
+++ javascript.js
@@ -4,32 +4,21 @@ const gridContainer = document.querySelector('.gridContainer');
 const style = document.createElement('style');
 document.head.appendChild(style)
 
+const elementsPerRow = 16;
+
 style.sheet.insertRule(`
 .gridItem {
-    width: 25%;
+    width: calc(100% / ${elementsPerRow});
     aspect-ratio: 1 / 1;
     border: 2px solid blue;
 }`);
 
-const gridElement1 = document.createElement('div');
-gridElement1.classList.add('gridItem');
-gridContainer.appendChild(gridElement1);
+for (let i = 0; i < elementsPerRow*elementsPerRow; i++) {
+    const gridElement1 = document.createElement('div');
+    gridElement1.classList.add('gridItem');
+    gridContainer.appendChild(gridElement1);
+}
 
-const gridElement2 = document.createElement('div');
-gridElement2.classList.add('gridItem')
-gridContainer.appendChild(gridElement2);
-
-const gridElement3 = document.createElement('div');
-gridElement3.setAttribute('class', 'gridItem');
-gridContainer.appendChild(gridElement3);
-
-const gridElement4 = document.createElement('div');
-gridElement4.className = 'gridItem';
-gridContainer.appendChild(gridElement4);
-
-const gridElement5 = document.createElement('div');
-gridElement5.classList.add('gridItem')
-gridContainer.appendChild(gridElement5);
 
 
 
