e94c8e811d0ee6bf90cc1ea186356812965a8738
odin-97 <220430078+odin-97@users.noreply.github.com>
2025-11-14 14:57:00 +0100

    Allows resizing of grid via button inputs.

--- javascript.js
+++ javascript.js
@@ -2,32 +2,62 @@ const gridContainer = document.querySelector('.gridContainer');
 const style = document.createElement('style');
 document.head.appendChild(style)
 
-const elementsPerRow = 16;
-style.sheet.insertRule(`
-.gridItem {
-    width: calc(100% / ${elementsPerRow});
-    aspect-ratio: 1 / 1;
-    border: 1px solid black;
-}`);
+let elementsPerRow = 16;
 
-for (let i = 0; i < elementsPerRow*elementsPerRow; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.classList.add('gridItem');
-    gridContainer.appendChild(gridElement);
-}
+function drawGrid(elementsPerRow) {
+    gridContainer.innerHTML = '';
+    for (let i = 0; i < elementsPerRow * elementsPerRow; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('gridItem');
+        gridContainer.appendChild(gridElement);
+    }
+} 
+
+drawGrid(elementsPerRow);
 
 let drawing = false
 document.addEventListener('mousedown', () => drawing = true);
 document.addEventListener('mouseup', () => drawing = false);
 
-const gridElementsAll = document.querySelectorAll('.gridItem');
-gridElementsAll.forEach(item => {
-    // item.style.backgroundColor = 'green';
-    item.addEventListener('mousedown', e => e.preventDefault());
+document.addEventListener('dragstart', e => e.preventDefault());
+document.addEventListener('selectstart', e => e.preventDefault());
 
-    item.addEventListener('mouseenter', () => {
-        if (drawing) item.style.backgroundColor = 'blue';
-    })
+gridContainer.addEventListener('mousedown', e => {
+    if (e.target.classList.contains('gridItem')) {
+        e.preventDefault(); 
+        e.target.style.backgroundColor = 'blue';   // paint first tile
+    }
+});
+gridContainer.addEventListener('mouseover', e => {
+    if (drawing && e.target.classList.contains('gridItem')) {
+        e.target.style.backgroundColor = 'blue';
+    }
+})
+
+
+const gridSize4x4 = document.querySelector('.button4');
+const gridSize16x16 = document.querySelector('.button16');
+const gridSize32x32 = document.querySelector('.button32');
+const gridSize64x64 = document.querySelector('.button64');
+
+gridSize4x4.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--gridSize', 4);
+    drawGrid(4);
+    console.log('Grid 4x4!')
+})
+
+gridSize16x16.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--gridSize', 16);
+    drawGrid(16);
+})
+
+gridSize32x32.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--gridSize', 32);
+    drawGrid(32);
+})
+gridSize64x64.addEventListener('click', () => {
+    document.documentElement.style.setProperty('--gridSize', 64);
+    drawGrid(64);
 })
 
 
