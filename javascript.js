ab64908bbf43036c36700d35b89bc3f872494f15
odin-97 <220430078+odin-97@users.noreply.github.com>
2025-11-14 12:26:44 +0100

    Add hover effect for coloring grid items
    Allows functionality to draw inside grid when mouse is pressed down.
    Colors grid in blue color when mouse enters  a grid item as long as
    mouse is pressed down.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,8 @@
 const gridContainer = document.querySelector('.gridContainer');
-
-
 const style = document.createElement('style');
 document.head.appendChild(style)
 
 const elementsPerRow = 16;
-
 style.sheet.insertRule(`
 .gridItem {
     width: calc(100% / ${elementsPerRow});
@@ -14,10 +11,22 @@ style.sheet.insertRule(`
 }`);
 
 for (let i = 0; i < elementsPerRow*elementsPerRow; i++) {
-    const gridElement1 = document.createElement('div');
-    gridElement1.classList.add('gridItem');
-    gridContainer.appendChild(gridElement1);
+    const gridElement = document.createElement('div');
+    gridElement.classList.add('gridItem');
+    gridContainer.appendChild(gridElement);
 }
+
+let drawing = false
+document.addEventListener('mousedown', () => drawing = true);
+document.addEventListener('mouseup', () => drawing = false);
+
+const gridElementsAll = document.querySelectorAll('.gridItem');
+gridElementsAll.forEach(item => {
+    // item.style.backgroundColor = 'green';
+    item.addEventListener('mouseenter', () => {
+        if (drawing) item.style.backgroundColor = 'blue';
+    })
+})
 
 
 
